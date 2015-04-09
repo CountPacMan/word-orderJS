34afe95c165995eae50c1ed0636ececf5bf7a3da
Daniel Toader & James Nielson <student@epicodus.com>
2015-04-09 12:09:31 -0700

    Add explanation comments.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,20 @@ function getWordOrder(input) {
   var word_order = {};
   var split_input = input.toLowerCase().split(' ');
 
+  // add words from split_input to the word_order object with the word as the key and the count as the value
   for (var i in split_input) {
     if (!word_order.hasOwnProperty(split_input[i])) {
-      word_order[split_input[i]] = 1;
+      word_order[split_input[i]] = 1; // ["word" => 1] kinda
     } else {
       word_order[split_input[i]]++;
     }
   }
+
   var sorted_words = [];
   for (var word in word_order) {
-    sorted_words.push([word, word_order[word]]);
+    sorted_words.push([word, word_order[word]]); // == ['bob', 1], ['jim', 3]
     sorted_words.sort(function(word1, word2) {
-      return word2[1] - word1[1];
+      return word2[1] - word1[1];  // What to sort by? not bob, but the val.
     });
   }
   var final = [];
@@ -30,7 +32,7 @@ jQuery(document).ready(function() {
   $("#word-order").submit(function(event) {
     var words = $("#words").val();
     var order = getWordOrder(words);
-    
+
     $("#order").text(order[0]);
     for (var i = 1; i < order.length; i++) {
       $("#order").append(", " + order[i]);
