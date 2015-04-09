cec6f9824a56e454f7e0d0c6e74fe64c2c00fc93
Daniel Toader & James Nielson <student@epicodus.com>
2015-04-09 11:26:43 -0700

    complete logic

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,18 @@
 describe('getWordOrder', function() {
   it("returns single word given", function() {
-    expect(getWordOrder()).to.equal();
+    expect(getWordOrder('bob')).to.deep.equal(['bob']);
   });
 
+  it("returns two different words given", function() {
+    expect(getWordOrder('bob dog')).to.deep.equal(['bob', 'dog']);
+  });
+
+  it("returns single word when given 2 of the same word", function() {
+    expect(getWordOrder('bob bob')).to.deep.equal(['bob']);
+  });
+
+  it("returns proper sorted word order", function() {
+    expect(getWordOrder('rug bob sog bob')).to.deep.equal(['bob',
+ 'rug', 'sog']);
+  });
 });
