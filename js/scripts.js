4ded6e0bf547bc4d22a933adf17b5918b8d51f95
Daniel Toader & James Nielson <student@epicodus.com>
2015-04-09 14:56:10 -0700

    fix sort

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,24 @@ function getWordOrder(input) {
   var word_order = {};
   var split_input = input.toLowerCase().split(' ');
 
-  // add words from split_input to the word_order object with the word as the key and the count as the value
-  for (var word in split_input) { // if word isn't in 'array' (object)...
-    if (!word_order.hasOwnProperty(split_input[word])) {
-      word_order[split_input[word]] = 1; // ...add it: ["word" => 1] kinda
+  for (var i in split_input) {
+    if (!word_order.hasOwnProperty(split_input[i])) {
+      word_order[split_input[i]] = 1;
     } else {
-      word_order[split_input[word]]++;
+      word_order[split_input[i]]++;
     }
   }
-["bob", 3], ["is", 2], ["when"]
+  
   var sorted_words = [];
   for (var word in word_order) {
-    sorted_words.push([word, word_order[word]]); //push(['bob', 1]) etc.
+    sorted_words.push([word, word_order[word]]);
+
   }
+
   sorted_words.sort(function(word1, word2) {
     return word2[1] - word1[1];
-    //sort by feeding function a & b and return result of b-a (descending, a-b ascending).
   });
+
   var final = [];
   for (var i in sorted_words) {
     final.push(sorted_words[i][0]);
