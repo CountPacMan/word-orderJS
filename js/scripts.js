e762a589fbb8f6269b4a8cb03bbdd131fa0cd098
Daniel Toader & James Nielson <student@epicodus.com>
2015-04-09 12:44:14 -0700

    Add explanation comments.

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,22 @@ function getWordOrder(input) {
   var split_input = input.toLowerCase().split(' ');
 
   // add words from split_input to the word_order object with the word as the key and the count as the value
-  for (var i in split_input) {
-    if (!word_order.hasOwnProperty(split_input[i])) {
-      word_order[split_input[i]] = 1; // ["word" => 1] kinda
+  for (var word in split_input) { // if word isn't in 'array' (object)...
+    if (!word_order.hasOwnProperty(split_input[word])) {
+      word_order[split_input[word]] = 1; // ...add it: ["word" => 1] kinda
     } else {
-      word_order[split_input[i]]++;
+      word_order[split_input[word]]++;
     }
   }
-
+["bob", 3], ["is", 2], ["when"]
   var sorted_words = [];
   for (var word in word_order) {
-    sorted_words.push([word, word_order[word]]); // == ['bob', 1], ['jim', 3]
-    sorted_words.sort(function(word1, word2) {
-      return word2[1] - word1[1];  // What to sort by? not bob, but the val.
-    });
+    sorted_words.push([word, word_order[word]]); //push(['bob', 1]) etc.
   }
+  sorted_words.sort(function(word1, word2) {
+    return word2[1] - word1[1];
+    //sort by feeding function a & b and return result of b-a (descending, a-b ascending).
+  });
   var final = [];
   for (var i in sorted_words) {
     final.push(sorted_words[i][0]);
